d57956117a8fed89a8154459f8eab3b9717d21ba
Aman <aman16024375@gmail.com>
2024-05-27 00:27:03 +0530

    Feat: Added JWT create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const {User} = require('./models/index');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('./repository/user-repository');
+//const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -19,12 +17,12 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        // const repo = new UserRepository();
-        // const response = await repo.getById(1);
-        // console.log(response);
-        // const incomingpassword = '123456';
-        // const user = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        
+        // const service = new UserService();
+        // const newToken = service.createToken({email: 'aman@gmail.com', id: 1});
+        // console.log("new token is", newToken);
+        // const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFtYW5AZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTcxNjc0OTM3MSwiZXhwIjoxNzE2ODM1NzcxfQ.wib8F4RPkiQ_ObZPTu8CZodQ3ULRhxjHzhM4RnQnduI';
+        // const response = service.verifyToken(token);
         // console.log(response);
     });
 }   
